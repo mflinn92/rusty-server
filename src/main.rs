e1ccb9cdd0066d337d23e3f05e3db268a9655324
mflinn92 <mflinn92@gmail.com>
2021-01-10 15:36:49 +0000

    more error handling

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,19 @@ use std::time::Duration;
 
 use hello::ThreadPool;
 
-
 static THREAD_COUNT: usize = 4;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let listener = match TcpListener::bind("127.0.0.1:7878") {
+        Ok(l) => l,
+        Err(e) => panic!("Unable to bind to TCP port: {:?}", e),
+    };
     let pool = match ThreadPool::new(THREAD_COUNT) {
         Ok(pool) => pool,
         Err(err) => panic!("error creating pool: {:?}", err),
     };
 
-    for stream in listener.incoming().take(2) {
+    for stream in listener.incoming() {
         let stream = stream.unwrap();
 
         match pool.execute(|| {
@@ -27,13 +29,15 @@ fn main() {
         };
     }
     println!("Shutting Down");
-
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
 
-    stream.read(&mut buffer).unwrap();
+    match stream.read(&mut buffer) {
+        Ok(_) => (),
+        Err(e) => println!("Unable to read from stream: {}", e),
+    }
 
     let get = b"GET / HTTP/1.1\r\n";
     let sleep = b"GET /sleep HTTP/1.1\r\n";
@@ -47,9 +51,16 @@ fn handle_connection(mut stream: TcpStream) {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
     };
 
-    let contents = fs::read_to_string(filename).unwrap();
+    let contents = match fs::read_to_string(filename) {
+        Ok(contents) => contents,
+        Err(e) => panic!("Error reading file: {}: {}", filename, e),
+    };
     let response = format!("{}{}", status_line, contents);
 
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    if let Err(e) = stream.write(response.as_bytes()) {
+        println!("Error writing to stream: {}", e);
+    };
+    if let Err(e) = stream.flush() {
+        println!("Unable to flush stream: {:?}", e);
+    };
 }
